1e03b4cd7c90bc9e08fb3ed6ec63473a0c1ca2e4
willy <willyanalvesgomes@hotmail.com>
2026-09-18 15:45:40 -0300

    Refactor restaurant status attribute to use a property

--- restaurant.py
+++ restaurant.py
@@ -4,16 +4,20 @@ class Restaurant:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.active = False
+        self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
         return f"{self.name.ljust(20)} | {self.category.ljust(20)}"
-
+    
     def list_restaurants():
         print(f'{"Nome do restaurante:".ljust(21)} |{"Categoria:".ljust(20)} |{"Status:"}')
         for restaurant in Restaurant.restaurants:
-            print(f'-{restaurant.name.ljust(20)} |{restaurant.category.ljust(20)} |{restaurant.active and "Ativo" or "Inativo"}')
+            print(f'-{restaurant.name.ljust(20)} |{restaurant.category.ljust(20)} |{restaurant.active}')
+
+    @property
+    def active(self):
+        return '✅' if self._active else '❌'
 
 restaurant_praca = Restaurant('Restaurante da Praça', 'Comida Caseira')
 restaurant_pizza = Restaurant('Pizzaria Express', 'Pizza')
