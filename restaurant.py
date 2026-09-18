dc16007f3c23468603dadcd94ad0f567b60af6d7
willy <willyanalvesgomes@hotmail.com>
2026-09-18 16:22:48 -0300

    Correct restaurant name attributes to use the private naming convention.

--- restaurant.py
+++ restaurant.py
@@ -2,18 +2,18 @@ class Restaurant:
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name.title()
+        self.category = category.upper()
         self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f"{self.name.ljust(20)} | {self.category.ljust(20)}"
+        return f"{self._name.ljust(20)} | {self.category.ljust(20)}"
     
     def list_restaurants():
         print(f'{"Nome do restaurante:".ljust(21)} |{"Categoria:".ljust(20)} |{"Status:"}')
         for restaurant in Restaurant.restaurants:
-            print(f'-{restaurant.name.ljust(20)} |{restaurant.category.ljust(20)} |{restaurant.active}')
+            print(f'-{restaurant._name.ljust(20)} |{restaurant.category.ljust(20)} |{restaurant.active}')
 
     @property
     def active(self):
